6931eac7955aac7f8caf59f4a551b977031e336b
webmasterlsw <129662390+webmasterlsw@users.noreply.github.com>
2024-03-02 17:36:40 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 151 ms, Memory: 20,168 KB -BaekjoonHub

--- SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
@@ -1,16 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Solution {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt();
-        for(int i = 1; i <= t; i++){
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            char res = '=';
-            if( a > b ) res = '>';
-            else if( a < b ) res = '<';
-            System.out.printf("#%d %c\n", i, res);
+public class Solution{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 테스트 실행횟수
+        int tc = Integer.parseInt(br.readLine());
+        // 테스트 실행횟수 숫자 올려주는?
+        for(int t = 1; t <= tc; t++){
+            //입력된 두 수
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int n1 = 0;
+            int n2 = 0;
+            // 입력된 토큰 나누기
+            n1 = Integer.parseInt(st.nextToken());
+            n2 = Integer.parseInt(st.nextToken());
+            //수 비교 연산자 비교하기
+            //연산자를 저장할 변수 선언
+            char op = ' ';
+            //두 수가 같다면 = 저장
+            if(n1 == n2){
+                op = '=';
+            }
+            else if(n1 > n2){
+                op = '>';
+            }
+            else {
+                op = '<';
+            }
+            //결과를 표시한다
+            System.out.printf("#%d %c%n", t, op);
         }
     }
 }
+
